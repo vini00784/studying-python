087c3b867251f407773e748283330b5a469ef1a7
vini00784 <vo70346@gmail.com>
2024-02-01 19:53:06 -0300

    third exercise

---
+++ exercises/ex003.py
@@ -0,0 +1,5 @@
+# Make a program that sum 2 numbers
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro númeo: '))
+sum = n1 + n2
+print(f'A soma entre {n1} e {n2} resulta em: {sum}')
