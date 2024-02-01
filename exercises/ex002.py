e31943a3a6d6903d357eb250ce8b05584ba6daa0
vini00784 <vo70346@gmail.com>
2024-02-01 19:02:00 -0300

    second exercise

---
+++ exercises/ex002.py
@@ -0,0 +1,5 @@
+# Make a program that reads a person's name and displays a welcome message
+name = input('Qual o seu nome?')
+print(f'Olá {name}, seja muito bem-vindo à plataforma!')
+
+# The "f-strings" work like the TypeScript template string, it is possible to call functions, variables, etc.
