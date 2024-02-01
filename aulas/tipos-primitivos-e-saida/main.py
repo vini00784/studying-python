84a42aa8811c1508be842493daebb73d69f831cd
vini00784 <vo70346@gmail.com>
2024-02-01 19:27:45 -0300

    learning about types

---
+++ aulas/tipos-primitivos-e-saida/main.py
@@ -0,0 +1,10 @@
+n1 = int(input("Digite um número: ")) # The "int()" function is necessary to indicate that what will be inserted into the input is a number
+n2 = int(input("Digite outro número: "))
+soma = n1 + n2
+print(f"A soma vale {n1 + n2}")
+
+# The primitive types in python are:
+    # int
+    # float
+    # bool
+    # str
