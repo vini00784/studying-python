153e7b0a7041aead452c645b6ea685673f400fbb
vini00784 <vo70346@gmail.com>
2024-02-06 08:07:45 -0300

    exercise to convert temperatures

---
+++ exercises/ex014.py
@@ -0,0 +1,7 @@
+# Write a program that converts a temperature from °C to °F
+
+cTemp = float(input('Insira uma temperatura em °C: '))
+
+fTemp = cTemp * 1.8 + 32
+
+print(f'Convertendo {cTemp}°C para Fahrenheint fica igual a {fTemp}°F')
