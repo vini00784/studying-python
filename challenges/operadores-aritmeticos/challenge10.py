ac4ab8c082381c598523c0376306a2ae6d4040b7
vini00784 <vo70346@gmail.com>
2024-02-06 07:42:38 -0300

    small changes

--- challenges/operadores-aritmeticos/challenge10.py
+++ challenges/operadores-aritmeticos/challenge10.py
@@ -1,8 +1,8 @@
 # Write a program that reads the amount of money a person has and how many dollars they can buy
 # US$1 = R$3.27
 
-value = float(input('Insira quanto você tem na conta: '))
+value = float(input('Insira quanto você tem na conta: R$'))
 
 dollars = value / 3.27
 
-print(f'Você tem {value} na carteira e pode comprar {dollars:.2f} dólares')
+print(f'Você tem R${value} na carteira e pode comprar US${dollars:.2f}')
