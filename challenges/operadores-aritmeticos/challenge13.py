7e61d20ac85807862b52d87372236ac746f8e7dd
vini00784 <vo70346@gmail.com>
2024-02-06 07:58:50 -0300

    small change

--- challenges/operadores-aritmeticos/challenge13.py
+++ challenges/operadores-aritmeticos/challenge13.py
@@ -1,7 +1,7 @@
 # Make a program that reads an employee's salary and shows their new salary with a 15% increase
 
-salary = int(input('Qual o seu salário atualmente? '))
+salary = float(input('Qual o seu salário atualmente? '))
 
 salaryWithIncrease = salary + salary * 0.15
 
-print(f'Seu salário atual é R${salary}. \nMas com um aumento de 15% ele vai ficar igual a R${salaryWithIncrease}, parábens')
+print(f'Seu salário atual é R${salary}. \nMas com um aumento de 15% ele vai ficar igual a R${salaryWithIncrease:.2f}, parábens!')
