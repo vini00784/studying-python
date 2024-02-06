2781a575e436a3d79d7be19f3bdcb3269fcef3a8
vini00784 <vo70346@gmail.com>
2024-02-06 07:10:39 -0300

    new exercise

---
+++ challenges/operadores-aritmeticos/challenge12.py
@@ -0,0 +1,7 @@
+# Make a program that reads a product price and displays a new price with 5% of discount
+
+price = float(input('Insira o valor do produto: '))
+
+priceWithDiscount = price - price * 0.05
+
+print(f'O valor original do produto é: R${price} \nCom um desconto de 5% fica: R${priceWithDiscount}')
