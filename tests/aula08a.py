633bac5cf83ed9934360ff5493b7c175f9826ecf
vini00784 <vo70346@gmail.com>
2024-02-06 10:15:26 -0300

    class to learn how import modules/libs

---
+++ tests/aula08a.py
@@ -0,0 +1,7 @@
+import math
+
+num = int(input('Digite um número inteiro: '))
+
+squareRoot = math.sqrt(num)
+
+print(f'A raíz quadrada de {num} é igual a {squareRoot:.2f}')
---
+++ tests/aula08b.py
@@ -0,0 +1,7 @@
+from math import sqrt
+
+num = int(input('Digite um número inteiro: '))
+
+squareRoot = sqrt(num)
+
+print(f'A raíz quadrada de {num} é igual a {squareRoot:.2f}')
