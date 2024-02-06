ad93c50558df42523fd820077a2bab3d4bde54fb
vini00784 <vo70346@gmail.com>
2024-02-06 07:30:03 -0300

    changing the exercise

--- challenges/operadores-aritmeticos/challenge08.py
+++ challenges/operadores-aritmeticos/challenge08.py
@@ -1,8 +1,18 @@
 # Write a program that takes a value in meters and converts it to centimeters and millimeters
 
-meterValue = int(input('Insira um valor em metros: '))
+meterValue = float(input('Insira um valor em metros: '))
 
+kilometerValue = meterValue / 1000
+hectometerValue = meterValue / 100
+decameterValue = meterValue / 10
+decimeterValue = meterValue * 10
 centimeterValue = meterValue * 100
 milimeterValue = meterValue * 1000
 
-print(f'O valor em centímetros é igual a: {centimeterValue} \nO valor em milímetros é igual a: {milimeterValue}')
+print(f'As conversões para {meterValue}m são:')
+print(f'{kilometerValue}km')
+print(f'{hectometerValue}hm')
+print(f'{decameterValue}dam')
+print(f'{decimeterValue}dm')
+print(f'{centimeterValue}cm')
+print(f'{milimeterValue}mm')
