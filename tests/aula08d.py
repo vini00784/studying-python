4737fa1e0d4385f9892df2c313265ad3b094a5a4
vini00784 <vo70346@gmail.com>
2024-02-22 18:11:30 -0300

    08 class

---
+++ tests/aula08d.py
@@ -0,0 +1,8 @@
+# Using the emoji lib
+# As it is an external lib, to install it, run the command" python -m pip install emoji"
+
+# python -m pip install lib_name
+
+import emoji
+
+print(emoji.emojize('Python is :thumbs_up:'))
